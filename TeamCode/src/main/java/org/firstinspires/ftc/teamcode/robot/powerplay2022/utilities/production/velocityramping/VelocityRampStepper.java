85cf2bfc769217ca27fb52abc33913a3eed61a60
icaras84 <icaras84@gmail.com>
2022-10-12 14:15:08 -0700

    VelocityRamping piecewise class?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/powerplay2022/utilities/production/velocityramping/VelocityRampStepper.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode.robot.powerplay2022.utilities.production.velocityramping;
+
+import java.util.ArrayList;
+
+public class VelocityRampStepper {
+    
+    public static class VelocityRampEquation{
+        private double accel, deceleration, maxAccel;
+        private double xInt;
+
+        public VelocityRampEquation(double maxAccel, double accel, double time){
+            this.maxAccel = maxAccel;
+            this.accel = accel;
+            this.deceleration = -accel;
+            this.xInt = time;
+        }
+
+        public double solve(double time){
+            return Math.min(Math.min(accel * time, deceleration * (time - xInt)), maxAccel);
+        }
+    }
+    
+    public VelocityRamping accelGenerator;
+    
+    private ArrayList<VelocityRampEquation> accelEquations;
+    public double estimatedElapsedTime, stateElapsedTime;
+    public int currentRamp;
+    
+    public VelocityRampStepper(){
+        this.accelGenerator = new VelocityRamping(1);
+        init();
+    }
+
+    public VelocityRampStepper(VelocityRamping velocityRamping){
+        this.accelGenerator = velocityRamping;
+        init();
+    }
+
+    private void init(){
+        accelEquations = new ArrayList<>();
+        estimatedElapsedTime = 0;
+        stateElapsedTime = 0;
+        currentRamp = 0;
+    }
+
+    public void addRamp(double distance, double time){
+        estimatedElapsedTime += time;
+        accelEquations.add(
+                new VelocityRampEquation(
+                        accelGenerator.MAX_VELOCITY,
+                        accelGenerator.solve(distance, time).acceleration,
+                        time
+                        )
+        );
+    }
+
+    public void addRamps(double[] distances, double[] times){
+        for (int i = 0; i < Math.min(distances.length, times.length); i++) {
+            estimatedElapsedTime += times[i];
+            accelEquations.add(
+                    new VelocityRampEquation(
+                            accelGenerator.MAX_VELOCITY,
+                            accelGenerator.solve(distances[i], times[i]).acceleration,
+                            times[i]
+                    )
+            );
+        }
+    }
+
+    public double update(double deltaTime){
+        stateElapsedTime += deltaTime;
+        if (stateElapsedTime >= accelEquations.get(currentRamp).xInt){
+            currentRamp++; //increment current ramp index
+            stateElapsedTime = 0; //reset current ramp's time in state
+        }
+        return accelEquations.get(currentRamp).solve(stateElapsedTime);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/powerplay2022/utilities/production/velocityramping/VelocityRamping.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/powerplay2022/utilities/production/velocityramping/VelocityRamping.java
@@ -5,12 +5,22 @@ public class VelocityRamping {
     public double MAX_VELOCITY;
     public double acceleration;
 
+    /**
+     * You must input a max velocity (in meters per second) in order to use it
+     * @param maxVel
+     */
     public VelocityRamping(double maxVel){
         this.MAX_VELOCITY = maxVel;
     }
 
-    public void solve(double distance, double time){
+    /**
+     * The solve function takes in two inputs: distance (m/s) and expected time (seconds)
+     * @param distance
+     * @param time
+     */
+    public VelocityRamping solve(double distance, double time){
         double distMax = Math.min((2 * distance) / time, MAX_VELOCITY);
         acceleration = distMax / (time - (distance / distMax));
+        return this;
     }
 }
