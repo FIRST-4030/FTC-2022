7397a115b18650151edb8ba41ae45aaa5abf32c1
icaras84 <icaras84@gmail.com>
2022-10-15 13:25:14 -0700

    ... *insert shrug*

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/powerplay2022/utilities/production/PowerPlayGlobals.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/powerplay2022/utilities/production/PowerPlayGlobals.java
@@ -3,5 +3,5 @@ package org.firstinspires.ftc.teamcode.robot.powerplay2022.utilities.production;
 public class PowerPlayGlobals {
 
     public static final double MAX_VELOCITY = 1.26930872781;
-    public static final double MAX_VEL = 0.367660858699;
+    public static final double MAX_VEL = 0.4052447792;
 }
