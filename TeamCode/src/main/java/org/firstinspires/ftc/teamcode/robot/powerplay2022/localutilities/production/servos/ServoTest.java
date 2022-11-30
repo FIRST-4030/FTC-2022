70fbc33a20eb42e725bc1e3140f0dc7760f58299
icaras84 <icaras84@gmail.com>
2022-11-29 16:47:54 -0800

    Servo Tester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/powerplay2022/localutilities/production/servos/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/powerplay2022/localutilities/production/servos/ServoTest.java
@@ -4,25 +4,50 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.extrautilslib.core.maths.vectors.Vector2d;
+import org.firstinspires.ftc.teamcode.extrautilslib.core.misc.EULConstants;
+import org.firstinspires.ftc.teamcode.robot.powerplay2022.localutilities.production.misc.InputAutoMapper;
 import org.firstinspires.ftc.teamcode.utils.actuators.ServoConfig;
 import org.firstinspires.ftc.teamcode.utils.actuators.ServoFTC;
+import org.firstinspires.ftc.teamcode.utils.gamepad.InputHandler;
 import org.firstinspires.ftc.teamcode.utils.momm.LoopUtil;
 
+import java.util.Arrays;
+
 @Config
-@TeleOp(name = "Servo Driver Test", group = "Tester")
+@TeleOp(name = "Multi Servo Tester", group = "Tester")
 public class ServoTest extends LoopUtil {
-    public static ServoFTC servo;
-    public static ServoConfig config;
+    public static ServoFTC[] servos;
+    public static ServoConfig[] configs;
+    public static double[] servoValues;
+
+    public static int servoSelector = 0;
+    public static InputHandler inputHandler;
     public static Vector2d joyStick = new Vector2d();
 
     @Override
     public void opInit() {
 
-        config = new ServoConfig("A",false, 0, 1);
-        servo = new ServoFTC(hardwareMap, telemetry, config);
+        configs = new ServoConfig[]{
+                new ServoConfig("A",false, 0.0001, 0.83),
+                new ServoConfig("B",true, 0.0001, 0.9999),
+                new ServoConfig("C",true, 0.0001, 0.9999),
+                new ServoConfig("D",true, 0.0001, 0.9999),
+                new ServoConfig("R",true, 0.0001, 0.9999)
+        };
+
+        servos = new ServoFTC[]{
+                new ServoFTC(hardwareMap, telemetry, configs[0]),
+                new ServoFTC(hardwareMap, telemetry, configs[1]),
+                new ServoFTC(hardwareMap, telemetry, configs[2]),
+                new ServoFTC(hardwareMap, telemetry, configs[3]),
+                new ServoFTC(hardwareMap, telemetry, configs[4])
+        };
+
+        servoValues = new double[servos.length];
+        Arrays.fill(servoValues, 0d);
+
         joyStick = new Vector2d(0, 1);
-
-
+        inputHandler = InputAutoMapper.normal.autoMap(this);
     }
 
     @Override
@@ -37,14 +62,42 @@ public class ServoTest extends LoopUtil {
 
     @Override
     public void opUpdate(double deltaTime) {
-        joyStick.x = -gamepad1.right_stick_x;
-        joyStick.y = gamepad1.right_stick_y;
-        servo.setPosition(Math.atan2(joyStick.y, joyStick.x));
+
     }
 
     @Override
     public void opFixedUpdate(double deltaTime) {
+        handleInput(deltaTime * EULConstants.MS2SEC);
 
+        for (int i = 0; i < servos.length; i++) {
+            servos[i].setPosition(servoValues[i]);
+        }
+    }
+
+    public void handleInput(double deltaTime){
+        inputHandler.loop();
+
+        if (inputHandler.up("D1:DPAD_RIGHT")){
+            servoSelector++;
+        }
+
+        if (inputHandler.up("D1:DPAD_LEFT")){
+            servoSelector--;
+        }
+
+        if (inputHandler.held("D1:DPAD_UP")){
+            servoValues[servoSelector] += 1 * deltaTime;
+        }
+
+        if (inputHandler.held("D1:DPAD_DOWN")){
+            servoValues[servoSelector] -= 1 * deltaTime;
+        }
+
+        servoSelector = Math.floorMod(servoSelector, servos.length);
+    }
+
+    public void handleTelemetry(){
+        telemetry.addData("CURRENT CONTROLLED SERVO: ", configs[servoSelector].name);
     }
 
     @Override
